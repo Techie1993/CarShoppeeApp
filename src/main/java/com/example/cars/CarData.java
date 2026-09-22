d49b015e09f81bb3d2cdfc7f1464168c5ca98b36
Techie1993 <akshayparhad21@gmail.com>
2026-09-22 11:32:03 +0530

    One class added

---
+++ src/main/java/com/example/cars/CarData.java
@@ -0,0 +1,10 @@
+package com.example.cars;
+
+public class CarData {
+
+	public static void main(String[] args) {
+		System.out.println("Car Data added");
+
+	}
+
+}
--- src/main/java/com/example/service/BookService.java
+++ src/main/java/com/example/service/BookService.java
@@ -16,4 +16,6 @@ public interface BookService {
 	
 	public void DeleteAll(); 
 	
+	
+	
 }
